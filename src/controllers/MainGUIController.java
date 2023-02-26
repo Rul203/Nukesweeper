a0e6eb0fa5769f70a973c898bb756686ffc3e66f
Raúl Correa Ocañas <rcorreao203@gmail.com>
2023-02-26 12:26:34 -0600

    temp

---
+++ src/controllers/MainGUIController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.layout.GridPane;
+
+public class MainGUIController {
+	
+	final int HEIGHT = 900;
+	final int WIDTH = 900;
+	
+	public Rectangle2D space;
+	public GridPane field;
+	
+	public void main(String[] args) {
+		
+	}
+	
+
+}
